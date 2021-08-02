1fa27f309c9d108b360645fe26abc0776c7c7ed2
Lukas C <lukascedronas1@gmail.com>
2021-08-02 22:47:38 +0300

    initial commit

---
+++ src/test/java/com/whatever/beerfinder/BeerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatever.beerfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
